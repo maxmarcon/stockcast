40318c9fb3198d5adeb2952760a41de512743555
Max Marcon <maxmarcon@gmx.net>
2020-05-13 21:45:33 +0200

    added first version of stock-viewer component

---
+++ stockcast_spa/src/components/stock-viewer.vue
@@ -0,0 +1,13 @@
+<template>
+    <b-card>
+        <template slot="header">
+            <!--            message-bar#errorBar(ref="errorBar" variant="danger" :seconds=10)-->
+            <b-row>
+                <b-col class="mt-1" md="6">
+                    Stocks here $$$
+                </b-col>
+            </b-row>
+        </template>
+        Stocks here $$$
+    </b-card>
+</template>
--- stockcast_spa/src/main.js
+++ stockcast_spa/src/main.js
@@ -1,8 +1,14 @@
+import StockViewer from './components/stock-viewer.vue'
 import Vue from 'vue'
-import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import './scss/main.scss'
 
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(StockViewer),
 }).$mount('#app')
