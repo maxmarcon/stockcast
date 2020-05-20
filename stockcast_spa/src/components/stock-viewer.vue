4aa46999e5801c674e517bd325661b5a10bada4f
Max Marcon <maxmarcon@gmx.net>
2020-05-20 23:59:27 +0200

    added date picker and tagged input

--- stockcast_spa/src/components/stock-viewer.vue
+++ stockcast_spa/src/components/stock-viewer.vue
@@ -1,13 +1,56 @@
 <template>
-    <b-card>
-        <template slot="header">
-            <!--            message-bar#errorBar(ref="errorBar" variant="danger" :seconds=10)-->
-            <b-row>
-                <b-col class="mt-1" md="6">
-                    Stocks here $$$
-                </b-col>
-            </b-row>
-        </template>
-        Money here $$$
-    </b-card>
+  <b-card>
+    <template slot="header">
+      <message-bar id="errorBar" ref="errorBar" variant="danger" :seconds=10></message-bar>
+      <b-form>
+        <b-form-row>
+          <b-col md="4">
+            <b-form-group label="Stocks:"
+                          labe-for="stocks">
+              <vue-tags-input
+                id="stocks"
+                v-model="tag"
+                :tags="tags"
+                @tags-changed="tagsChanged"
+                :avoidAddingDuplicates="true"
+                :autocomplete-items="autocompleteItems"
+                :max-tags="10"
+                :maxlength="50"
+                :add-only-from-autocomplete="true"
+                placeholder="Search by name, ticker, or ISIN">
+              </vue-tags-input>
+            </b-form-group>
+          </b-col>
+          <b-col md="auto">
+            <b-form-group label="From:"
+                          labe-for="date-from">
+              <b-form-datepicker id="date-from"></b-form-datepicker>
+            </b-form-group>
+          </b-col>
+          <b-col md="auto">
+            <b-form-group label="To:"
+                          labe-for="date-to">
+              <b-form-datepicker id="date-to"></b-form-datepicker>
+            </b-form-group>
+          </b-col>
+        </b-form-row>
+      </b-form>
+    </template>
+    Money here $$$
+  </b-card>
 </template>
+<script>
+  export default {
+    data: () => ({
+      tag: '',
+      tags: [],
+      autocompleteItems: []
+    }),
+    methods: {
+      tagsChanged() {
+      }
+    }
+  }
+</script>
+
+
--- stockcast_spa/src/main.js
+++ stockcast_spa/src/main.js
@@ -1,12 +1,18 @@
-import StockViewer from '@/components/stock-viewer.vue'
 import Vue from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueTagsInput from '@johmun/vue-tags-input';
 import './scss/main.scss'
+
+import StockViewer from '@/components/stock-viewer.vue'
+import MessageBar from '@/components/message-bar.vue'
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 Vue.config.productionTip = false
+
+Vue.component('vueTagsInput', VueTagsInput)
+Vue.component('messageBar', MessageBar)
 
 new Vue({
   render: h => h(StockViewer),
