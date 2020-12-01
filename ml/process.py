0345e5fa4a5bc3ca1ca4711befb761fc0fae9189
Max Marcon <maxmarcon@gmx.net>
2020-12-01 22:32:55 +0100

    added matplotlyb

--- ml/process.py
+++ ml/process.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import tensorflow.keras as keras
 import tensorflow.keras.layers as layers
-
+import matplotlib.pyplot as plt
 
 def make_sets(array, training, validation):
     array_size = len(array)
@@ -20,8 +20,8 @@ model = keras.Sequential([
     layers.Input((None, 2)),
     # needs to feed sequences and not only the last output to the next layer
     layers.BatchNormalization(),
-    layers.LSTM(10, return_sequences=True),
-    layers.LSTM(10),
+    layers.LSTM(50, return_sequences=True),
+    layers.LSTM(50),
     # next five days
     layers.Dense(output_size)
 ])
@@ -29,9 +29,9 @@ model = keras.Sequential([
 features, labels = [], []
 sequence_length = 60
 
-for i in range(0, input_data.shape[0]-(sequence_length+output_size)+1):
-    features.append(input_data[i:i+sequence_length])
-    labels.append(input_data[i+sequence_length:i+sequence_length+output_size, 1])
+for i in range(0, input_data.shape[0] - (sequence_length + output_size) + 1):
+    features.append(input_data[i:i + sequence_length])
+    labels.append(input_data[i + sequence_length:i + sequence_length + output_size, 1])
 
 print("Feature set has size {}".format(len(features)))
 
@@ -39,8 +39,8 @@ features, labels = np.array(features), np.array(labels)
 
 x_train, x_val, x_test = make_sets(features, training_size, validation_size)
 y_train, y_val, y_test = make_sets(labels, training_size, validation_size)
-# check out model.fit validation_split option... maybe we don't need an explicit validation set
 
-print("Training set has size {}, validation set has size {}, test set has size {}".format(len(x_train), len(x_val), len(x_test)))
+print("Training set has size {}, validation set has size {}, test set has size {}".format(len(x_train), len(x_val),
+                                                                                          len(x_test)))
 
-model.compile(optimizer = 'sgd', loss = 'mean_squared_error')
+model.compile(optimizer='sgd', loss='mean_squared_error', metrics=['accuracy'])
