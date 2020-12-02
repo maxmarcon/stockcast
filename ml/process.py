261b2f5d8449a600a09add87bcdc0d6d46ca239e
Max Marcon <maxmarcon@gmx.net>
2020-12-02 22:50:06 +0100

    added plotting

--- ml/process.py
+++ ml/process.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as pyplot
 import numpy as np
 import pandas as pd
 import tensorflow.keras as keras
@@ -9,11 +10,13 @@ def make_sets(array, training, validation):
     return np.split(array, (int(array_size * training), int(array_size * (training + validation))))
 
 
-feature_columns = ['day_of_week', 'close']
+feature_columns = ['close']
 sequence_length = 60
 output_size = 5
 training_size = 0.7
 validation_size = 0.15
+epochs = 100
+plot_results_for_test = 1
 
 data = pd.read_csv('prices-AMZ-GY-2020-01-01-2020-11-21.csv')
 label_columns = ['close']
@@ -24,6 +27,8 @@ model = keras.Sequential([
     layers.Input((None, len(feature_columns))),
     # needs to feed sequences and not only the last output to the next layer
     layers.BatchNormalization(),
+    layers.LSTM(50, return_sequences=True),
+    layers.LSTM(50, return_sequences=True),
     layers.LSTM(50, return_sequences=True),
     layers.LSTM(50),
     # next five days
@@ -48,4 +53,19 @@ y_train, y_val, y_test = make_sets(labels, training_size, validation_size)
 print("Training set has size {}, validation set has size {}, test set has size {}".format(len(x_train), len(x_val),
                                                                                           len(x_test)))
 
-model.compile(optimizer='sgd', loss='mean_squared_error', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mean_squared_error'])
+
+model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=200, batch_size=x_train.shape[0])
+
+
+for r in map(lambda a, b: a + ": " + str(b), model.metrics_names, model.test_on_batch(x_test, y_test)):
+    print(r)
+    
+predicted = model.predict(x_test[plot_results_for_test].reshape(1, 60, len(feature_columns)))
+    
+    
+pyplot.plot(range(0, x_test.shape[1]), x_test[plot_results_for_test, :, 1], color='blue', label='Real prices')
+pyplot.plot(range(x_test.shape[1], x_test.shape[1] + 5), y_test[plot_results_for_test], color='blue')
+pyplot.plot(range(x_test.shape[1], x_test.shape[1] + 5), predicted[0], color='red', label='Predicted prices')
+pyplot.legend()
+pyplot.show()
